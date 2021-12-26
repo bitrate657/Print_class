895338bd08d0efcaa737c60f40ffe5459af77104
bitrate657 <tgdegberyi@gmail.com>
2021-12-26 21:23:09 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Print_class
-C++の競プロ用printクラス
+C++の競プロ用printクラスです。scolさんのhttps://scol.hatenablog.com/entry/2020/09/14/215726
+を参考に作成しました。  
+scolさんのものとの差異  
+・sepとtermを実行時に選択できる機能を削除  
+・vectorや2次元vectorを出力するときに要素数を指定しなかった場合はすべて出力するように  
+・コンストラクタ呼び出し時に標準出力と標準エラー出力のどちらに出力するか選択できるように
+
+このプログラムは MIT License のもとで公開されています。
+ただし、競技プログラミングのためにこのプログラムの一部をジャッジサーバなどに送信するとき、
+著作権表示および許諾表示を省略することができます。
+
+使用例
+```cpp
+// include は省略
+using namespace std; // 前提 
+
+//ここにmain.hppの中身
+
+Print print(true); // trueを選択すると標準出力
+Print debug(false); // falseを選択すると標準エラー出力
+
+int main() {
+    // 1 abc a
+    print(1, "abc", 'a'); // 標準出力
+    debug(1, "abc", 'a'); // 標準エラー出力
+    
+    vector<int> vec(3);
+    // 0 0 0
+    print.pvec(vec);
+    // 0 0
+    print.pvec(vec, 2); // 第2引数に要素数を指定するとその要素数ぶんだけ出力
+    
+    vector<vector<int>> mat(3, vector<int>> (3));
+    // 0 0 0
+    // 0 0 0
+    // 0 0 0
+    print.pmat(mat);
+    // 0
+    // 0
+    print.pmat(mat, 2, 1); // 第2、第3引数に要素数を指定するとその要素数ぶんだけ出力
+}
+```
+
---
+++ main.hpp
@@ -0,0 +1,103 @@
+class Print {
+public:
+    Print(const bool is_cout) : is_cout(is_cout) {} // trueならcout falseならcerr
+
+    //要素数を省略した場合は全部出力
+    template<typename T>
+    void pvec(const T &vec) {
+        size_t sz = vec.size();
+        if (is_cout) {
+            for (size_t i = 0; i < sz; ++i)
+                cout << vec[i] << (i == sz - 1 ? term : sep);
+            flush(cout);
+        } else {
+            for (size_t i = 0; i < sz; ++i)
+                cerr << vec[i] << (i == sz - 1 ? term : sep);
+            flush(cerr);
+        }
+    }
+
+    template<typename T>
+    void pvec(const T &vec, size_t sz) {
+        if (is_cout)
+            for (size_t i = 0; i < sz; ++i) {
+                cout << vec[i] << (i == sz - 1 ? term : sep);
+                flush(cout);
+            } else {
+            for (size_t i = 0; i < sz; ++i)
+                cerr << vec[i] << (i == sz - 1 ? term : sep);
+            flush(cerr);
+        }
+    }
+
+    template<typename T>
+    void pmat(const T &mat) {
+        size_t h = mat.size();
+        size_t w = mat[0].size();//mat.size() > 0 を仮定
+        if (is_cout) {
+            for (size_t i = 0; i < h; ++i)
+                for (size_t j = 0; j < w; j++)
+                    cout << mat[i][j] << (j == w - 1 ? term : sep);
+            flush(cout);
+        } else {
+            for (size_t i = 0; i < h; ++i)
+                for (size_t j = 0; j < w; j++)
+                    cerr << mat[i][j] << (j == w - 1 ? term : sep);
+            flush(cerr);
+        }
+    }
+
+    template<typename T>
+    void pmat(const T &mat, size_t h, size_t w) {
+        if (is_cout) {
+            for (size_t i = 0; i < h; ++i)
+                for (size_t j = 0; j < w; j++)
+                    cout << mat[i][j] << (j == w - 1 ? term : sep);
+            flush(cout);
+        } else {
+            for (size_t i = 0; i < h; ++i)
+                for (size_t j = 0; j < w; j++)
+                    cerr << mat[i][j] << (j == w - 1 ? term : sep);
+            flush(cerr);
+        }
+    }
+
+    template<typename... Args>
+    void operator()(Args... args) const {
+        print(args...);
+        if (is_cout)
+            flush(cout);
+        else
+            flush(cerr);
+    }
+
+private:
+    bool is_cout;
+    string sep = " ";
+    string term = "\n";
+
+    template<typename Head, typename... Tail>
+    void print(Head &&head, Tail &&... tail) const {
+        if (is_cout)
+            cout << head;
+        else
+            cerr << head;
+        print2(forward<Tail>(tail)...);
+    }
+
+    template<typename Head, typename...Tail>
+    void print2(Head &&head, Tail &&... tail) const {
+        if (is_cout)
+            cout << sep << head;
+        else
+            cerr << sep << head;
+        print2(forward<Tail>(tail)...);
+    }
+
+    void print2() const {
+        if (is_cout)
+            cout << term;
+        else
+            cerr << term;
+    }
+};
